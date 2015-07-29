b1a9cb687e3370e6b4edc78d3146351448e0b07a
bchevallereau <benjamin.chevallereau@gmail.com>
2015-07-29 11:59:56 +0200

    add more information in the messages

--- 00-download.sh
+++ 00-download.sh
@@ -1,6 +1,7 @@
 # Check if the Ephesoft linux installer has been downloaded
 if [ ! -f $INSTALLER ]
   then
-    echo "Downloading ephesoft..."
+    echo "Downloading ephesoft... The file is about 810MB large, so you'll need some patience..."
+    echo "You can download, as well, ephesoft manually from this URL (http://www.ephesoft.com/Ephesoft_Product/Ephesoft_Community_4.0.2.0_Linux_10_July_2015/Ephesoft_Community_Release_4.0.2.0.zip), and place it in the same folder as your Vagrant file"
     wget -q http://www.ephesoft.com/Ephesoft_Product/Ephesoft_Community_4.0.2.0_Linux_10_July_2015/Ephesoft_Community_Release_4.0.2.0.zip >> /var/log/ephesoft-install.log
 fi
