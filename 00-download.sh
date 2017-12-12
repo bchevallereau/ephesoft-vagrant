9caedbab05800116cffb28540b0370fb28c88c2f
Ben Chevallereau <benjamin.chevallereau@gmail.com>
2017-12-12 18:45:24 -0500

    bchevallereau/ephesoft-vagrant#1

--- 00-download.sh
+++ 00-download.sh
@@ -2,6 +2,6 @@
 if [ ! -f $INSTALLER ]
   then
     echo "Downloading ephesoft... The file is about 810MB large, so you'll need some patience..."
-    echo "You can download, as well, ephesoft manually from this URL (http://www.ephesoft.com/Ephesoft_Product/Ephesoft_Community_4.0.2.0_Linux_10_July_2015/Ephesoft_Community_Release_4.0.2.0.zip), and place it in the same folder as your Vagrant file"
-    wget -q http://www.ephesoft.com/Ephesoft_Product/Ephesoft_Community_4.0.2.0_Linux_10_July_2015/Ephesoft_Community_Release_4.0.2.0.zip >> /var/log/ephesoft-install.log
+    echo "You can download, as well, ephesoft manually from this URL (http://download.ephesoft.com/Ephesoft_Product/Ephesoft_Community_4.0.2.0/Ephesoft_Community_Release_4.0.2.0.zip), and place it in the same folder as your Vagrant file"
+    wget -q http://download.ephesoft.com/Ephesoft_Product/Ephesoft_Community_4.0.2.0/Ephesoft_Community_Release_4.0.2.0.zip >> /var/log/ephesoft-install.log
 fi
--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure(2) do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "chef/centos-6.5"
+  config.vm.box = "puphpet/centos65-x64"
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
